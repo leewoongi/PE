aca0cedd267f1ff41af7b39e474a3f9c90494274
leewoongi <t2korea@naver.com>
2020-11-10 06:55:18 +0900

    feat(#5) : 내 정보

--- app/src/main/java/com/experiencers/playeasy/view/myinformation/MyInfoContract.java
+++ app/src/main/java/com/experiencers/playeasy/view/myinformation/MyInfoContract.java
@@ -1,4 +1,26 @@
 package com.experiencers.playeasy.view.myinformation;
 
+import com.experiencers.playeasy.view.base.BaseContract;
+
 public interface MyInfoContract {
+    interface view extends BaseContract.view{
+        @Override
+        void init();
+
+        @Override
+        void changeActivity();
+
+        @Override
+        void showResult();
+    }
+
+    interface presenter extends BaseContract.presenter<view>{
+        @Override
+        void setView(view view);
+
+        void confirmButtonClick(int itemId);
+
+        @Override
+        void deleteView();
+    }
 }
--- app/src/main/java/com/experiencers/playeasy/view/myinformation/MyInfoPresenter.java
+++ app/src/main/java/com/experiencers/playeasy/view/myinformation/MyInfoPresenter.java
@@ -1,4 +1,30 @@
 package com.experiencers.playeasy.view.myinformation;
 
-public class MyInfoPresenter {
+import com.experiencers.playeasy.R;
+
+public class MyInfoPresenter implements MyInfoContract.presenter{
+
+    private MyInfoContract.view view;
+    public MyInfoPresenter() {
+    }
+
+    @Override
+    public void setView(MyInfoContract.view view) {
+        this.view = view;
+    }
+
+    @Override
+    public void confirmButtonClick(int itemId) {
+        switch (itemId) {
+            case R.id.confirm_button:
+                // 서버 통신 부분
+                view.changeActivity();
+                break;
+        }
+    }
+
+    @Override
+    public void deleteView() {
+        view = null;
+    }
 }
