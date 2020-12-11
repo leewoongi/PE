8a4bc3b3dc8de357bb3bc72a1641c734c1b42ed1
leewoongi <t2korea@naver.com>
2020-12-11 18:24:50 +0900

    refact(#14) : glide + 내 정보 이미지 넣기

--- app/src/main/java/com/experiencers/playeasy/view/myinformation/MyInfoActivity.java
+++ app/src/main/java/com/experiencers/playeasy/view/myinformation/MyInfoActivity.java
@@ -1,6 +1,7 @@
 package com.experiencers.playeasy.view.myinformation;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.experiencers.playeasy.R;
 import com.experiencers.playeasy.application.TokenManger;
 import com.experiencers.playeasy.model.entity.User;
@@ -87,8 +89,10 @@ public class MyInfoActivity extends AppCompatActivity implements MyInfoContract.
         User myInfo = (User) object;
         Log.d("myInfo",myInfo.toString());
 
-        /* Todo 이미지는 글라이드 */
+        Uri userImageUri = Uri.parse(myInfo.getPicture());
 
+        /* Todo 이미지는 글라이드 */
+        Glide.with(this).load(userImageUri).into(myImage);
         myEmail.setText(myInfo.getEmail());
         myName.setText(myInfo.getName());
         myAge.setText(String.valueOf(myInfo.getAge()));
